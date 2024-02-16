1de1361ca2d38bb5b7e73bbe7bd498ca75d1eb58
javidSahabin <159636001+javidSahabin@users.noreply.github.com>
2024-02-16 09:26:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+ScrollReveal({ 
+    reset: true,
+    distance: '60px',
+    duration: 1500,
+    delay: 100              
+});
+
+ScrollReveal().reveal('.anim-1', { delay: 500, origin: 'top' });
+ScrollReveal().reveal('.anim-2', { delay: 600, origin: 'right' });
+ScrollReveal().reveal('.img-box-1', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.anim-b', { delay: 700, origin: 'left' });
+ScrollReveal().reveal('.anim-c', { delay: 900, origin: 'left' });
+ScrollReveal().reveal('.anim-d', { delay: 1100, origin: 'left' });
